7943b197fd6a63ece1d49da43d08823266275674
Platon <platon@MacBook-Pro-Platon.local>
2024-03-08 17:05:22 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -4,30 +4,46 @@ const tg = window.Telegram.WebApp;
 // function App() {
 //   return (
 //       <div classname = "App">
-//         привет ариветеп
+//         привет ариuuyyuyuyuyuyuuyuyuyuyuyuyuветеп
 //       </div>
 //   );
 // }
 //
 // export default App;
 
-
 function App() {
-    const [button1Clicked, setButton1Clicked] = useState(false);
-    const [button2Clicked, setButton2Clicked] = useState(false);
-    const [button3Clicked, setButton3Clicked] = useState(false);
+
+    // useEffect( () => {
+    //         tg.ready();
+    //     }, [])
+    const onClose = () => {
+        tg.close()
+    }
 
     return (
-        <div>
-            <button onClick={() => setButton1Clicked(true)}>Button 1</button>
-            <button onClick={() => setButton2Clicked(true)}>Button 2</button>
-            <button onClick={() => setButton3Clicked(true)}>Button 3</button>
-
-            {button1Clicked && <p>Button 1 was clicked!</p>}
-            {button2Clicked && <p>Button 2 was clicked!</p>}
-            {button3Clicked && <p>Button 3 was clicked!</p>}
+        <div className="App">
+            сайт сайт сайт
+            <button onClick={onClose}>Закрыть эту хрень</button>
         </div>
-    );
+    )
 }
-
 export default App;
+
+// function App() {
+//     const [button1Clicked, setButton1Clicked] = useState(false);
+//     const [button2Clicked, setButton2Clicked] = useState(false);
+//     const [button3Clicked, setButton3Clicked] = useState(false);
+//
+//     return (
+//         <div>
+//             <button onClick={() => setButton1Clicked(true)}>Button 1</button>
+//             <button onClick={() => setButton2Clicked(true)}>Button 2</button>
+//             <button onClick={() => setButton3Clicked(true)}>Button 3</button>
+//
+//             {button1Clicked && <p>Button 1 was clicked!</p>}
+//             {button2Clicked && <p>Button 2 was clicked!</p>}
+//             {button3Clicked && <p>Button 3 was clicked!</p>}
+//         </div>
+//     );
+// }
+// export default App;
